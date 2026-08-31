3eccc54682578ad6ed5b7e2a262afc4905e0d30d
spuschg-max <s.puschg@gmail.com>
2026-08-31 09:07:47 +0200

    Offline-Cache um Verbformen erweitern

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='russisch-vokabeltrainer-v5';
-const ASSETS=['./','./index.html','./styles.css','./data.js','./app.js','./features.js','./speaking.js','./manifest.webmanifest','./app-icon.svg'];
+const CACHE='russisch-vokabeltrainer-v6';
+const ASSETS=['./','./index.html','./styles.css','./data.js','./app.js','./features.js','./speaking.js','./forms.js','./manifest.webmanifest','./app-icon.svg'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match('./index.html'))))});
