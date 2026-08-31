4fa731c7ef903b314657e36bd73cefd0f62d1404
spuschg-max <s.puschg@gmail.com>
2026-08-31 15:42:13 +0200

    Refresh cache for language-aware speech recognition

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='russisch-vokabeltrainer-v26';
+const CACHE='russisch-vokabeltrainer-v27';
 const ASSETS=['./','./index.html','./styles.css','./data.js','./app.js','./features.js','./speaking.js','./forms.js','./audio-toggle.js','./stable-voice.js','./forms-voice.js','./update-helper.js','./exercise-packages.js','./import-code.js','./standard-a1a2-data.js','./standard-b1-data.js','./standard-pack.js','./manifest.webmanifest','./app-icon.svg'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
