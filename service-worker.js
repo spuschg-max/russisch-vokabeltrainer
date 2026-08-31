27e6c9cddeea5555d1594e2438c771decec9ae16
spuschg-max <s.puschg@gmail.com>
2026-08-31 08:20:24 +0200

    Offline-Cache für neue App-Funktionen aktualisieren

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='russisch-vokabeltrainer-v1';
-const ASSETS=['./','./index.html','./styles.css','./data.js','./app.js','./manifest.webmanifest'];
+const CACHE='russisch-vokabeltrainer-v2';
+const ASSETS=['./','./index.html','./styles.css','./data.js','./app.js','./features.js','./manifest.webmanifest','./app-icon.svg'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match('./index.html'))))});
