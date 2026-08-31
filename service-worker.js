d10b5ced4e43c535b5aacee77d08b3096b296168
spuschg-max <s.puschg@gmail.com>
2026-08-31 13:08:05 +0200

    Cache global sound toggle

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='russisch-vokabeltrainer-v21';
-const ASSETS=['./','./index.html','./styles.css','./data.js','./app.js','./features.js','./speaking.js','./forms.js','./mic-recovery.js','./feedback-recovery.js','./forms-voice.js','./update-helper.js','./exercise-packages.js','./import-code.js','./standard-a1a2-data.js','./standard-b1-data.js','./standard-pack.js','./manifest.webmanifest','./app-icon.svg'];
+const CACHE='russisch-vokabeltrainer-v22';
+const ASSETS=['./','./index.html','./styles.css','./data.js','./app.js','./features.js','./speaking.js','./forms.js','./mic-recovery.js','./feedback-recovery.js','./audio-toggle.js','./forms-voice.js','./update-helper.js','./exercise-packages.js','./import-code.js','./standard-a1a2-data.js','./standard-b1-data.js','./standard-pack.js','./manifest.webmanifest','./app-icon.svg'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',event=>{
