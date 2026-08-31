e6d15da08d00970db9d7879c8cd28c65eaddab82
spuschg-max <s.puschg@gmail.com>
2026-08-31 08:39:53 +0200

    Offline-Cache für Übungen und Spracheingabe aktualisieren

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='russisch-vokabeltrainer-v2';
+const CACHE='russisch-vokabeltrainer-v3';
 const ASSETS=['./','./index.html','./styles.css','./data.js','./app.js','./features.js','./manifest.webmanifest','./app-icon.svg'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
