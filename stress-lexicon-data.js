57e8d609c96ad63156505c75e2636fab9a87a5d6
spuschg-max <s.puschg@gmail.com>
2026-09-02 13:48:19 +0200

    Add fallback stress lexicon file

---
+++ stress-lexicon-data.js
@@ -0,0 +1,2 @@
+window.RVT_STRESS_LEXICON=window.RVT_STRESS_LEXICON||{};
+window.RVT_STRESS_META=window.RVT_STRESS_META||{source:'OpenRussian / Russian Dictionary Data',license:'CC BY-SA 4.0',entries:0,ambiguousOmitted:0,rowsRead:0};
