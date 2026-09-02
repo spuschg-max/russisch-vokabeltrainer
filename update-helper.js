b5ded745f928d663e9e14df5e1200dec46fb337e
spuschg-max <s.puschg@gmail.com>
2026-09-02 11:15:29 +0200

    Load conjugation study audio panel

--- update-helper.js
+++ update-helper.js
@@ -1,6 +1,6 @@
 (() => {
 'use strict';
-const BUILD='2026.09.02.35';
+const BUILD='2026.09.02.36';
 const $=s=>document.querySelector(s);
 let lastCheck=0;
 
@@ -26,6 +26,7 @@ async function loadHelpers(){
   loadScript('reveal-answer.js','reveal-answer');
   loadScript('problem-vocab.js','problem-vocab');
   loadScript('conjugation-drill.js','conjugation-drill');
+  loadScript('conjugation-study.js','conjugation-study');
   await loadScript('audio-toggle.js','audio-toggle');
   await loadScript('speech-unlock.js','speech-unlock');
   await loadScript('speech-tolerance.js','speech-tolerance');
