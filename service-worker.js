85fca4e12f15e1d70bea8477b3e6f7c5d0223753
spuschg-max <s.puschg@gmail.com>
2026-09-03 07:22:19 +0200

    Force clean cache for Sep 1 evening rollback

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='russisch-vokabeltrainer-v42';
+const CACHE='russisch-vokabeltrainer-v84';
 const ASSETS=['./','./index.html','./styles.css','./data.js','./app.js','./features.js','./speaking.js','./forms.js','./audio-toggle.js','./speech-unlock.js','./speech-tolerance.js','./speech-segmentation.js','./speech-start-guard.js','./voice-controller.js','./voice-selfcheck.js','./voice-add.js','./learning-ui.js','./motion-hints.js','./reveal-answer.js','./problem-vocab.js','./forms-voice.js','./update-helper.js','./exercise-packages.js','./import-code.js','./standard-a1a2-data.js','./standard-b1-data.js','./standard-pack.js','./manifest.webmanifest','./app-icon.svg'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
